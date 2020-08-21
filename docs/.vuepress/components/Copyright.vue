791790f97bc667ff2064bd62af9681cbeaf7427c
wuyang <wayne05@126.com>
2020-08-21 17:30:02 +0800

    modify copyright

--- docs/.vuepress/components/Copyright.vue
+++ docs/.vuepress/components/Copyright.vue
@@ -4,10 +4,7 @@
         <p>
 <br>
 —————————— <br>
-作者：zhāngpéngjūn <br>
-链接：<a :href="location">{{location}}</a> <br>
-来源：znote <br>
-版权声明：著作权归原作者所有，商业转载请联系作者获得授权，非商业转载请注明出处。
+内容链接：<a :href="location">{{location}}</a>
         </p>
     </div>
 </template>
