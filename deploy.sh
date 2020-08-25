f16a28927aea7971264819048dd780ffd4fe9611
wuyang <wayne05@126.com>
2020-08-25 15:29:22 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -5,34 +5,37 @@
 set -e
 
 # 生成静态文件
-npm run docs:build
+#npm run docs:build
 
 # 进入生成的文件夹
-cd docs/.vuepress/dist/
+#cd docs/.vuepress/dist/
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
+#Gitee 部署
 git init
 git add -A
 git commit -m 'deploy'
-git remote add origin git@github.com:waynehoo/doc.git
-git checkout -b gh-pages
+git remote add origin git@gitee.com:wayne05/doc.git
+#git checkout -b gh-pages
 
 # 如果发布到 https://<USERNAME>.github.io
 #git push -f git@github.com:waynehoo/waynehoo.github.io.git gh-pages
 #git push origin :gh-pages
 git push -u origin gh-pages -f
 
-cd ..
+
+# 更新github和gitee的master分支
+#cd ..
 #git init
 #git remote add origin git@github.com:waynehoo/doc.git
-git add .
-git commit -m 'update docs' 
-git push origin master
+#git add .
+#git commit -m 'update docs' 
+#git push origin master
 
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
-cd -
+#cd -
